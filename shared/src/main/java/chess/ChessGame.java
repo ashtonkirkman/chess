3980f36895c586b3438cafdff1fd8a1ac4d75675
ashtonkirkman <akirkman@byu.edu>
2024-02-06 13:21:42 -0700

    Changed the parseAlgebraicMoves method to make moves as it reads in the notation.

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -3,6 +3,8 @@ package chess;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Objects;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * For a class that can manage a chess game, making moves on a board
@@ -15,23 +17,25 @@ public class ChessGame {
     private TeamColor teamTurn;
     private ChessBoard board;
 
+    private final ChessPosition[] a = new ChessPosition[9];
+    private final ChessPosition[] b = new ChessPosition[9];
+    private final ChessPosition[] c = new ChessPosition[9];
+    private final ChessPosition[] d = new ChessPosition[9];
+    private final ChessPosition[] e = new ChessPosition[9];
+    private final ChessPosition[] f = new ChessPosition[9];
+    private final ChessPosition[] g = new ChessPosition[9];
+    private final ChessPosition[] h = new ChessPosition[9];
+
     public ChessGame() {
+        this.initializeChessPositions();
         this.teamTurn = TeamColor.WHITE;
         this.board = new ChessBoard();
         this.board.resetBoard();
     }
 
-    public static void main(String[] args) {
-        ChessPosition[] a = new ChessPosition[9];
-        ChessPosition[] b = new ChessPosition[9];
-        ChessPosition[] c = new ChessPosition[9];
-        ChessPosition[] d = new ChessPosition[9];
-        ChessPosition[] e = new ChessPosition[9];
-        ChessPosition[] f = new ChessPosition[9];
-        ChessPosition[] g = new ChessPosition[9];
-        ChessPosition[] h = new ChessPosition[9];
+    private void initializeChessPositions() {
         a[0] = null; b[0] = null; c[0] = null; d[0] = null; e[0] = null; f[0] = null; g[0] = null; h[0] = null;
-        for(int i = 1; i < 9; i++) {
+        for (int i = 1; i < 9; i++) {
             a[i] = new ChessPosition(i, 1);
             b[i] = new ChessPosition(i, 2);
             c[i] = new ChessPosition(i, 3);
@@ -41,39 +45,16 @@ public class ChessGame {
             g[i] = new ChessPosition(i, 7);
             h[i] = new ChessPosition(i, 8);
         }
+    }
+
+    public static void main(String[] args) {
+
 
         ChessGame myGame = new ChessGame();
         ChessBoard currentBoard = myGame.getBoard();
-        currentBoard.clearBoard();
-        myGame.setBoard(currentBoard);
         System.out.println(myGame);
 
-        currentBoard.addPiece(b[6], new ChessPiece(TeamColor.WHITE, ChessPiece.PieceType.PAWN));
-        myGame.setBoard(currentBoard);
-        System.out.println(myGame);
-
-        currentBoard.addPiece(e[6], new ChessPiece(TeamColor.WHITE, ChessPiece.PieceType.PAWN));
-        myGame.setBoard(currentBoard);
-        System.out.println(myGame);
-
-        currentBoard.addPiece(e[7], new ChessPiece(TeamColor.WHITE, ChessPiece.PieceType.PAWN));
-        myGame.setBoard(currentBoard);
-        System.out.println(myGame);
-
-        currentBoard.addPiece(f[6], new ChessPiece(TeamColor.WHITE, ChessPiece.PieceType.PAWN));
-        myGame.setBoard(currentBoard);
-        System.out.println(myGame);
-
-        currentBoard.addPiece(d[8], new ChessPiece(TeamColor.BLACK, ChessPiece.PieceType.KING));
-        myGame.setBoard(currentBoard);
-        System.out.println(myGame);
-
-        try {
-            myGame.makeMove(new ChessMove(e[7], e[8], ChessPiece.PieceType.BISHOP));
-        } catch (InvalidMoveException ex) {
-            System.out.println(ex.getMessage());
-        }
-
+        myGame.parseAlgebraicMoves("f3 e6 g4 Qh4");
         System.out.println(myGame);
     }
 
@@ -82,6 +63,14 @@ public class ChessGame {
      */
     public TeamColor getTeamTurn() {
         return this.teamTurn;
+    }
+
+    private void attemptToMakeMove(ChessMove move) {
+        try {
+            this.makeMove(move);
+        } catch (InvalidMoveException ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 
     /**
@@ -326,6 +315,128 @@ public class ChessGame {
         return board;
     }
 
+    private void checkToAddMove(ChessPiece.PieceType pieceType, ChessPosition endPosition) {
+
+        ChessPosition[][] currentPosition = new ChessPosition[8][8];
+        ChessPiece[][] currentPiece = new ChessPiece[8][8];
+        ChessPosition startPosition = null;
+        boolean moveAdded = false;
+
+        for(int i = 0; i < 8; i++) {
+            if(moveAdded) {
+                break;
+            }
+            for(int j = 0; j < 8; j++){
+                currentPosition[i][j] = new ChessPosition(i+1, j+1);
+                currentPiece[i][j] = board.getPiece(currentPosition[i][j]);
+                if(currentPiece[i][j] == null) {
+                    continue;
+                }
+                if(currentPiece[i][j].getPieceType() == pieceType && currentPiece[i][j].getTeamColor() == teamTurn) {
+                    startPosition = currentPosition[i][j];
+                    if (validMoves(startPosition).contains(new ChessMove(startPosition, endPosition, null))) {
+                        attemptToMakeMove(new ChessMove(startPosition, endPosition, null));
+                        moveAdded = true;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    private void parseAlgebraicMoves(String moveString) {
+        String[] moveTokens = moveString.split("\\s*\\d+\\.\\s*");
+        ChessPosition startPosition = new ChessPosition(1, 1);
+        ChessPosition endPosition = null;
+
+        ChessPosition[][] currentPosition = new ChessPosition[8][8];
+        ChessPiece[][] currentPiece = new ChessPiece[8][8];
+
+//        for (String token : moveTokens) {
+//            System.out.println(token);
+//        }
+
+        for(String token : moveTokens) {
+            String[] moveParts = token.trim().split("\\s+");
+            for(String part : moveParts) {
+//                System.out.println(part);
+
+                if(part.matches("[KQRBN]x?[a-h][1-8]")) {
+
+                    int rank = part.matches("[KQRBN][a-h][1-8]") ? Character.getNumericValue(part.charAt(2)) : Character.getNumericValue(part.charAt(3));
+                    int file = part.matches("[KQRBN][a-h][1-8]") ? part.charAt(1) - 'a' + 1 : part.charAt(2) - 'a' + 1;
+
+                    char piece = part.charAt(0);
+
+                    endPosition = new ChessPosition(rank, file);
+
+                    switch(piece) {
+                        case 'K':
+                            checkToAddMove(ChessPiece.PieceType.KING, endPosition);
+                            break;
+                        case 'Q':
+                            checkToAddMove(ChessPiece.PieceType.QUEEN, endPosition);
+                            break;
+                        case 'R':
+                            checkToAddMove(ChessPiece.PieceType.ROOK, endPosition);
+                            break;
+                        case 'B':
+                            checkToAddMove(ChessPiece.PieceType.BISHOP, endPosition);
+                            break;
+                        case 'N':
+                            checkToAddMove(ChessPiece.PieceType.KNIGHT, endPosition);
+                            break;
+                    }
+                }
+                else if(part.matches("[a-h][1-8]")) {
+                    int rank = Character.getNumericValue(part.charAt(1));
+                    int file = part.charAt(0) - 'a' + 1;
+                    endPosition = new ChessPosition(rank, file);
+
+                    for(int i = 1; i < 7; i++) {
+                        currentPosition[i][file-1] = new ChessPosition(i+1, file);
+                        currentPiece[i][file-1] = board.getPiece(currentPosition[i][file-1]);
+                        if(currentPiece[i][file-1] == null) {
+                            continue;
+                        }
+                        if(currentPiece[i][file-1].getPieceType() == ChessPiece.PieceType.PAWN && currentPiece[i][file-1].getTeamColor() == teamTurn) {
+                            startPosition = currentPosition[i][file-1];
+                            if (validMoves(startPosition).contains(new ChessMove(startPosition, endPosition, null))) {
+                                attemptToMakeMove(new ChessMove(startPosition, endPosition, null));
+                                break;
+                            }
+                        }
+                    }
+                }
+                else if(part.matches("[a-h]x[a-h][1-8]")) {
+                    int rank = Character.getNumericValue(part.charAt(3));
+                    int file = part.charAt(2) - 'a' + 1;
+                    endPosition = new ChessPosition(rank, file);
+
+                    int startFile = part.charAt(0) - 'a' + 1;
+
+                    for(int i = 1; i < 7; i++) {
+                        currentPosition[i][startFile-1] = new ChessPosition(i+1, startFile);
+                        currentPiece[i][startFile-1] = board.getPiece(currentPosition[i][startFile-1]);
+                        if(currentPiece[i][startFile-1] == null) {
+                            continue;
+                        }
+                        if(currentPiece[i][startFile-1].getPieceType() == ChessPiece.PieceType.PAWN && currentPiece[i][startFile-1].getTeamColor() == teamTurn) {
+                            startPosition = currentPosition[i][startFile-1];
+                            if (validMoves(startPosition).contains(new ChessMove(startPosition, endPosition, null))) {
+                                attemptToMakeMove(new ChessMove(startPosition, endPosition, null));
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+//        for(ChessMove move : moves) {
+//            System.out.println(move);
+//        }
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -340,15 +451,15 @@ public class ChessGame {
 
     @Override
     public String toString() {
-//        if(isInCheckmate(TeamColor.WHITE)) {
-//            return "Black Wins!\n\n" + board;
-//        }
-//        else if(isInCheckmate(TeamColor.BLACK)) {
-//            return "White Wins!\n\n" + board;
-//        }
-//        else if(isInStalemate(TeamColor.WHITE) || isInStalemate(TeamColor.BLACK)) {
-//            return "Stalemate!\n\n" + board;
-//        }
+        if(isInCheckmate(TeamColor.WHITE)) {
+            return "Black Wins!\n\n" + board;
+        }
+        else if(isInCheckmate(TeamColor.BLACK)) {
+            return "White Wins!\n\n" + board;
+        }
+        else if(isInStalemate(TeamColor.WHITE) || isInStalemate(TeamColor.BLACK)) {
+            return "Stalemate!\n\n" + board;
+        }
 
         return teamTurn + " to Play\n\n" + board;
     }
