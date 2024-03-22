3aba38ef5f9c7e143c03e0d375a992f4014fefc1
ashtonkirkman <akirkman@byu.edu>
2024-03-22 02:28:12 -0600

    Fixed and passed all tests

--- client/src/test/java/clientTests/ServerFacadeTests.java
+++ client/src/test/java/clientTests/ServerFacadeTests.java
@@ -18,7 +18,7 @@ public class ServerFacadeTests {
     @BeforeAll
     public static void init() throws ResponseException, IOException {
         server = new Server();
-        var port = server.run(8080);
+        var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
         serverFacade = new ServerFacade("http://localhost:8080");
     }
